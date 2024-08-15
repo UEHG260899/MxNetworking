f40c3252c43cb7b5261872bd83b397ea1b84c334
UEHG260899 <67031721+UEHG260899@users.noreply.github.com>
2024-08-14 18:39:20 -0600

    Adding async data method (#10)
    * Adding async data method and first tests
    
    * Finishing async data method
    
    * Adding documenttion to async method

--- MxNetworking/MxNetworking/API/MxNetworker.swift
+++ MxNetworking/MxNetworking/API/MxNetworker.swift
@@ -126,6 +126,12 @@ public class MxNetworker {
         }
     }
 
+    
+    /// Executes a network request and completes with `Data` in order to perform custom decodings
+    /// or any other custom logic with it
+    /// - Parameters:
+    ///   - request: Object containing the request information
+    ///   - completion: Handler to be called once the request completes
     public func data(for request: Request, completion: @MainActor @escaping (Result<Data, APIError>) -> Void) {
         guard let urlRequest = request.httpRequest() else {
             Task {
@@ -143,13 +149,11 @@ public class MxNetworker {
                 }
             }
             
-            
             if let error {
                 result = .failure(.unknown(description: error.localizedDescription))
                 return
             }
-            
-            
+
             guard let httpResponse = response as? HTTPURLResponse else {
                 result = .failure(.invalidResponse(response: response))
                 return
@@ -168,5 +172,27 @@ public class MxNetworker {
             result = .success(data)
             
         }.resume()
+    }
+
+    /// Executes a network request and returns with `Data` in order to perform custom decodings
+    /// or any other custom logic with it
+    /// - Parameter request: Object containing the request information
+    /// - Returns: The `Data` received from the request
+    public func data(for request: Request) async throws -> Data {
+        guard let urlRequest = request.httpRequest() else {
+            throw APIError.invalidRequest
+        }
+
+        let (data, response) = try await session.data(for: urlRequest)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw APIError.invalidResponse(response: response)
+        }
+    
+        guard (200...300) ~= httpResponse.statusCode else {
+            throw APIError.requestFailed(errorCode: httpResponse.statusCode)
+        }
+        
+        return data
     }
 }
